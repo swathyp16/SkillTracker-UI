87768a7f33ccaf0cf3e6ea0c1ad2bb319a328015
unknown <swathy.p16@gmail.com>
2018-05-24 16:41:13 +0530

    skill ui

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule , Routes} from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { HomeComponent } from './home/home.component';
+import { AddSkillComponent } from './add-skill/add-skill.component';
+const appRoutes: Routes = [
+  {path : 'home', component: HomeComponent},
+  {path : '', component: HomeComponent},//redirectTo: '/app',pathMatch: 'full'},
+  {path : 'searchAssociate',  component: SearchComponent},
+  {path : 'addSkill', component: AddSkillComponent}
+  // {path : 'editWorkout/:id', component: EditWorkoutComponent},
+  // {path : 'startWorkout/:id', component: StartEndWorkoutComponent},
+  //{path : '**', component: PagenotfoundComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchComponent,
+    HomeComponent,
+    AddSkillComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+  ],
+  exports: [RouterModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
