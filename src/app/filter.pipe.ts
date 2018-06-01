b250b4434b4ac07ecd9324553c6c392a1755ef73
unknown <swathy.p16@gmail.com>
2018-06-01 16:04:41 +0530

    Display associate skills as comma separated values

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -5,10 +5,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
- transform(associateData: any, filter: any, searchByName?: any,searchByAssociateId?:any,searchByEmail?:any,searchByMobile?:any): any {
+ transform(associateData: any, filter: any, searchByName?: any,searchByAssociateId?:any,searchByEmail?:any,searchByMobile?:any,searchByStrongskills?:any): any {
    
     if (associateData && associateData.length){
       return associateData.filter(data =>{
+          let flag = false;
           if (searchByName && data.name.toLowerCase().indexOf(searchByName.toLowerCase()) === -1){
               return false;
           }
@@ -21,6 +22,14 @@ export class FilterPipe implements PipeTransform {
           if (searchByMobile && data.mobileNum.toString().indexOf(searchByMobile.toString()) === -1){
             return false;
           }
+          for(var i = 0; i<data.associateSkills.length; i++){
+            if (searchByStrongskills && data.associateSkills[i].skillName.toLowerCase().includes(searchByStrongskills.toLowerCase())){
+               flag = true;
+            }
+          }   
+          if(flag == true){
+            return false;
+          }      
           return true;
     })
   }
