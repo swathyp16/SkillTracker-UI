1812f17dbb1bcc83830a98e45ae884e3d9f10fe5
unknown <swathy.p16@gmail.com>
2018-06-04 14:43:14 +0530

    Skill graph

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -27,7 +27,8 @@ export class SearchComponent implements OnInit {
   maleCandidatesReg:number=0;
   femaleCandidatesPercentage:number=0;
   maleCandidatesPercentage:number=0;
-  skillData:Array<any>;
+  //skillData:Array<any>;
+  //skillData : number[] = [10,20,30];
   constructor(private _searchService : SearchService,
     private _sharedService : SharedService,private _addAssociateService : AddAssociateService) { }
 
@@ -37,28 +38,98 @@ export class SearchComponent implements OnInit {
       this.associateData = data;
       this.candidatesRegistered = this.associateData.length;
       this.calculateCandidateDetails();
-      this.displaySkillGraph();
+     // this.displaySkillGraph();
     });
     
   }
 
   displaySkillGraph(){
-    this.skillData.push()
+    //this.skillData.push()
   }
 
+  skillData = [{
+    data: [727, 589, 537, 543, 574],
+    backgroundColor: "rgba(63,103,126,1)",
+    hoverBackgroundColor: "rgba(50,90,100,1)"
+},{
+    data: [238, 553, 746, 884, 903],
+    backgroundColor: "rgba(163,103,126,1)",
+    hoverBackgroundColor: "rgba(140,85,100,1)"
+},{
+    data: [1238, 553, 746, 884, 903],
+    backgroundColor: "rgba(63,203,226,1)",
+    hoverBackgroundColor: "rgba(46,185,235,1)"
+}]
+
+  chartcolor = [
+    { 
+    backgroundColor: 'rgba(30, 169, 224 , 0.8)'
+    }
+]
+
   chartOptions = {
-    responsive: true,
-    maintainAspectRatio: false,
-    // scales : {
-    //     xAxes: [{
-    //        ticks: {
-    //           //steps : 10,
-    //           stepValue : 200,
-    //           //max : 2000,
-    //         }
-    //     }] 
-    //   }
-}
+	responsive: true,
+    tooltips: {
+        enabled: false
+    },
+    hover :{
+        animationDuration:0
+    },
+    scales: {
+        xAxes: [{
+            ticks: {
+                beginAtZero:true,
+                fontFamily: "'Open Sans Bold', sans-serif",
+                fontSize:11
+            },
+            scaleLabel:{
+                display:false
+            },
+            gridLines: {
+            }, 
+            stacked: true
+        }],
+        yAxes: [{
+            gridLines: {
+                display:false,
+                color: "#fff",
+                zeroLineColor: "#fff",
+                zeroLineWidth: 0
+            },
+            ticks: {
+                fontFamily: "'Open Sans Bold', sans-serif",
+                fontSize:11
+            },
+            stacked: true
+        }]
+    },
+    legend:{
+        display:false
+    },
+    
+    animation: {
+        onComplete: function () {
+            var chartInstance = this.chart;
+            var ctx = chartInstance.ctx;
+            ctx.textAlign = "left";
+            ctx.font = "9px Open Sans";
+            ctx.fillStyle = "#fff";
+
+            Chart.helpers.each(this.data.datasets.forEach(function (dataset, i) {
+                var meta = chartInstance.controller.getDatasetMeta(i);
+                Chart.helpers.each(meta.data.forEach(function (bar, index) {
+                    data = dataset.data[index];
+                    if(i==0){
+                        ctx.fillText(data, 50, bar._model.y+4);
+                    } else {
+                        ctx.fillText(data, bar._model.x-25, bar._model.y+4);
+                    }
+                }),this)
+            }),this);
+        }
+    },
+    pointLabelFontFamily : "Quadon Extra Bold",
+    scaleFontFamily : "Quadon Extra Bold",
 
   calculateCandidateDetails(){
     for(var i=0; i<this.associateData.length;i++){
