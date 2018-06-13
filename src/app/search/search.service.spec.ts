cdb0c43aeca3bd2369e8e9ab1618fb711d7e06e1
unknown <swathy.p16@gmail.com>
2018-06-14 02:12:09 +0530

    Karma test cases

--- src/app/search/search.service.spec.ts
+++ src/app/search/search.service.spec.ts
@@ -9,20 +9,51 @@ import { SearchService } from './search.service';
 let spyHttp: jasmine.SpyObj<Http>;
 let service: SearchService;
 
-const associateList = [{
-  "associateId":14526,"name":"Arun",
-  "email":"arun.naik@gmail.com","mobile":"8745999854","gender":"male",
-  "statusGreen":true,"statusBlue":false,"statusRed":false,"level1":true,"level2":false,
-  "level3":false,"remark":"Good","strength":"UI","weakness":"java",
-  "associateSkills":[{"skillId":1,"skillName":"HTML5","skillRating":16,"isEdit":false},
-  {"skillId":2,"skillName":"CSS3","skillRating":15,"isEdit":false}],
-  "otherSkill":"jquery"},{"associateId":87451,"name":"Tom","email":"tom.george@gmail.com",
-"mobile":"7412589630","gender":"male","statusGreen":false,"statusBlue":true,"statusRed":false,
-"level1":false,"level2":false,"level3":true,"remark":"Satisfactory","strength":"React",
-"weakness":"node", "associateSkills":[{"skillId":1,"skillName":"HTML5","skillRating":12},
-{"skillId":10,"skillName":"Spring","skillRating":9,"isEdit":false},
-{"skillId":11,"skillName":"Spring MVC","skillRating":17},
-{"skillId":17,"skillName":"Hidernate","skillRate":20}],"otherSkill":"Other"}];
+const associateList = [
+  {
+  "associateId":14526,
+  "name":"Arun",
+  "email":"arun.naik@gmail.com",
+  "mobile":"8745999854",
+  "gender":"male",
+  "statusGreen":true,
+  "statusBlue":false,
+  "statusRed":false,
+  "level1":true,
+  "level2":false,
+  "level3":false,
+  "remark":"Good",
+  "strength":"UI",
+  "weakness":"java",
+  "associateSkills":[
+    {"skillId":1,"skillName":"HTML5","skillRating":16,"isEdit":false},
+    {"skillId":2,"skillName":"CSS3","skillRating":15,"isEdit":false}
+  ],
+  "otherSkill":"jquery"
+},
+{
+  "associateId":87451,
+  "name":"Tom",
+  "email":"tom.george@gmail.com",
+  "mobile":"7412589630",
+  "gender":"male",
+  "statusGreen":false,
+  "statusBlue":true,
+  "statusRed":false,
+  "level1":false,
+  "level2":false,
+  "level3":true,
+  "remark":"Satisfactory",
+  "strength":"React",
+  "weakness":"node",
+  "associateSkills":[
+    {"skillId":1,"skillName":"HTML5","skillRating":12},
+    {"skillId":10,"skillName":"Spring","skillRating":9,"isEdit":false},
+    {"skillId":11,"skillName":"Spring MVC","skillRating":17},
+    {"skillId":17,"skillName":"Hidernate","skillRate":20}
+  ],
+"otherSkill":"Other"
+}];
 
 describe('SearchService', () => {
   beforeEach(() => {
